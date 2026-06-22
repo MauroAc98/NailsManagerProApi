3b78dc92ee1dc4a8f8ca0e38f612e571b5a219a4
macosta <acostamaurohernan98@gmail.com>
2026-06-22 19:33:39 -0300

    agregando tabla settings para configuraciones de admin

---
+++ app/Models/Setting.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    protected $fillable = [
+        'key',
+        'value',
+        'description',
+    ];
+
+    public static function get(string $key): ?string
+    {
+        return static::where('key', $key)->value('value');
+    }
+}
---
+++ database/seeders/SettingsSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SettingsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $settings = [
+            [
+                'key'         => 'support_whatsapp',
+                'value'       => '+5493764794897',
+                'description' => 'Número de WhatsApp de soporte',
+            ],
+            [
+                'key'         => 'support_email',
+                'value'       => 'nailsmanagerpro.app@gmail.com',
+                'description' => 'Email de soporte',
+            ],
+        ];
+
+        foreach ($settings as $setting) {
+            \App\Models\Setting::updateOrCreate(
+                ['key' => $setting['key']],
+                $setting,
+            );
+        }
+    }
+}
